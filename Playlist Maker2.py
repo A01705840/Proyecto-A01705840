633537d9acaf652e7006b523481891a07b3eee86
Maria Jose Soto Castro <111139686+A01705840@users.noreply.github.com>
2022-09-22 16:51:18 -0500

    Add files via upload

--- Playlist Maker2.py
+++ Playlist Maker2.py
@@ -1,3 +1,4 @@
+
 """
 Proyecto Creador de Playlists
 El programa reliza una búsqueda de artísta
@@ -7,6 +8,7 @@ Se pueden retirar y añadir canciones.
 #La Bilbioteca que usaré más tarde se llama Spotipy.
 #tuve que registrarme como desarrolladora para tener un cliend ID y usar la biblioteca
 #Con la biblioteca, quiero poner más artistas y canciones y hacer playlist de Spotify
+
 play_list = [ ]
 lista_artistas = ["Imagine Dragons" , "Harry Styles", "Alejandro Fernández",\
     "Kishi Bashi", "Hans Zimmer", "Lorde"]
@@ -25,91 +27,70 @@ artista_3 = str(input("Dime tu último artista: "))
 def busca_canciones(artista_1, artista_2, artista_3):
     global play_list
     while len(play_list) < 15:
-        if artista_1  == "Imagine Dragons" or artista_2   == "Imagine Dragons" or artista_3  == "Imagine Dragons" \
-           or "imagine dragons":
-            play_list.append("Believer")
-            play_list.append("Thunder")
-            play_list.append("Natural")
-            play_list.append("Whatever It Takes")
-            play_list.append("Radioactive")
-        if artista_1 == "Harry Styles" or artista_2 == "Harry Styles" or artista_3  == "Harry Styles" or "harry styles":
-            play_list.append("Adore You")
-            play_list.append("Falling")
-            play_list.append("Lights Up")
-            play_list.append("Sign of Times")
-            play_list.append("Watermelon Sugar")
-        if artista_1 == "Alejandro Fernandez" or artista_2 == "Alejandro Fernandez"  or artista_3 == "Alejandro Fernandez"\
-           or "alejandro fenandez":
-            play_list.append("Amor de los Dos")
-            play_list.append("No")
-            play_list.append("Nube Viajera")
-            play_list.append("Abrázame")
-            play_list.append("Mátalas")
-        if artista_1 == "Kishi Bashi" or artista_2 == "Kishi Bashi" or artista_3 == "Kishi Bashi"\
-           or "kishi bashi":
-            play_list.append("I am the Anti-Christ to you")
-            play_list.append("Honeybody")
-            play_list.append("This Must Be The Place")
-            play_list.append("Can`t let go Juno")
-            play_list.append("Bright Whites")
-        if artista_1  == "Hans Zimmer" or artista_2  == "Hans Zimmer" or artista_3 == "Hans Zimmer"\
-           or "hans zimmer":
-            play_list.append("Cornfield Chase")
-            play_list.append("Time")
-            play_list.append("Main Titles: Top Gun Maverick")
-            play_list.append("Day One")
-            play_list.append("Darkstar")
-        if artista_1  == "Lorde" or artista_2  == "Lorde" or artista_3 == "Lorde"\
-           or "lorde":
-            play_list.append("Ribs")
-            play_list.append("Royals")
-            play_list.append("Team")
-            play_list.append("Liability")
-            play_list.append("Green Light")
+        if artista_1  == "Imagine Dragons" or "imagine dragons" or artista_2   == "Imagine Dragons" or "imagine dragons" or artista_3  == "Imagine Dragons"\
+        or artista_3  =="imagine dragons":
+            play_list.extend(["Believer","Thunder","Natural","Whatever It Takes", "Radioactive"])
+        if artista_1 == "Harry Styles"   or artista_1 == "harry styles"or artista_2 == "Harry Styles"  or artista_2 == "harry styles" or artista_3 == "Harry Styles"\
+        or artista_3 =="harry styles":
+            play_list.extend(["Adore You","Falling","Lights Up","Sign of Times", "Watermelon Sugar"])
+        if artista_1 == "Alejandro Fernandez"   or artista_1 == "alejandro fernandez"or artista_2 == "Alejandro Fernandez"\
+        or artista_2 == "alejandro fernandez" or artista_3 == "Alejandro Fernandez" or artista_3 =="alejandro fernandez":
+            play_list.extend(["Amor de los Dos", "No", "Nube Viajera", "Abrázame", "Mátalas"])
+        if artista_1 == "Kishi Bashi" or artista_1 == "kishi bashi"or artista_2 == "Kishi Bashi" or artista_2 == "kishi bashi" or artista_3 == "Kishi Bashi"\
+        or artista_3 == "kishi bashi":
+            play_list.extend(["I am the Anti-Christ to you", "Honeybody", "This Must Be The Place","Can't let go Juno","Bright Whites"])
+        if artista_1  == "Hans Zimmer" or artista_1  == "hans zimmer" or artista_2  == "Hans Zimmer" or artista_2  == "hans zimmer" or artista_3 == "Hans Zimmer"\
+        or artista_3  == "hans zimmer":
+            play_list.extend(["Cornfield Chase", "Time", "Main Titles: Top Gun Maverick", "Day One", "Darkstar"])
+        if artista_1  == "Lorde"  or artista_1  == "lorde" or artista_2  == "Lorde"  or artista_2  == "lorde" or artista_3 ==  "Lorde"\
+        or artista_3  == "lorde":
+            play_list.extend(["Ribs", "Royals", "Team","Liability","Green Light"])
         else:
             print("Lo siento, no hay datos del artista :(")
-    return play_list
 
 def nueva_playlist(nueva_canc):
+    global play_list
     play_list.append(nueva_canc)
-    return play_list
-
+    for v in play_list:
+            Canciones = v
+            print("{:<8}".format(Canciones))
+    add_canc = "%"
 def quitar_cancion(no_canc):
-    play_list.remove(no_canc)
-    return play_list
- 
+    global play_list
+    if no_canc in play_list:
+        play_list.remove(no_canc)
+        for v in play_list:
+                Canciones = v
+                print("{:<8}".format(Canciones))
+    else:
+        print("La canción no está en esta playlist")
+    
 busca_canciones(artista_1, artista_2, artista_3)
-print("{:<8}".format('Canciones'))
-
 for v in play_list:
     Canciones = v
     print ("{:<8}".format(Canciones))
     
-while True:    #Junté las funciones de añadir y quitar canciones en solo una pregunta
-    print("¿Añadir otra canción? - A")
-    print("¿Quitar una canción?-Q")
-    print("Si deseas salir: - Exit")
-    add_canc = input()
+print("¿Añadir otra canción? - A")
+print("¿Quitar una canción?-Q")
+print("Si deseas salir: - Exit")
+add_canc = input()
+
+while add_canc != "Exit" and add_canc != "exit": #Cambié el while True con break al final por un while que terminara con la palabra Exit. 
     if add_canc == "A" or add_canc == "a":
         nueva_canc = input("Escribe canción: ")
-        print("Tu nueva playlist es ")
+        print("Tu nueva playlist es: ")
         nueva_playlist(nueva_canc)
-        print("{:<8}".format('Canciones'))
-        
-        for v in play_list:
-            Canciones = v
-            print ("{:<8}".format(Canciones))
-        continue
-    elif add_canc == "Q" or add_canc == "q":
+        add_canc = "%"
+    if add_canc == "Q" or add_canc == "q":
         no_canc = input("¿Cuál canción? ")
         print("Tu nueva playlist es: ")
         quitar_cancion(no_canc)
-        print("{:<8}".format('Canciones'))
-
-        for v in play_list:
-            Canciones = v
-            print ("{:<8}".format(Canciones))
-        continue
+        add_canc = "%"
+    if add_canc == "%":
+        print("¿Añadir otra canción? - A")
+        print("¿Quitar una canción?-Q")
+        print("Si deseas salir: - Exit")
+        add_canc = input()
     else:
-        print("OK")
-        break
+        print("Por favor, ingrese una de las tres opciones")
+        add_canc = "%"
