4b2c98c3cb7a3de57f5efbe99784cbbd4d578f56
Maria Jose Soto Castro <111139686+A01705840@users.noreply.github.com>
2022-10-13 22:37:49 -0500

    Add files via upload

--- Playlist Maker2.py
+++ Playlist Maker2.py
@@ -1,4 +1,3 @@
-import random
 """
 Proyecto Creador de Playlists
 El programa reliza una búsqueda de artísta
@@ -6,8 +5,15 @@ y pone en una playlist, las canciones más famosas.
 Se pueden retirar y añadir canciones.
 """
 
+'''--------------------------CREACIÓN DE PLAYLIST------------------------------'''
 def busca_canciones(artista_1, artista_2, artista_3,play_list):
-    while len(play_list) < 15:
+     """
+    (uso de condiciones, uso de funciones)
+    recibe: asrtista_1, artista_2, artista_3 (cadena) y play_list (lista)
+    busca los tres artistas y añade las top 5 canciones a la lista play_list
+    devuelve: playlist con las top 5 canciones de los 3 artistas
+    """
+     while len(play_list) < 15:
         if artista_1  == "Imagine Dragons" or "imagine dragons" or artista_2   == "Imagine Dragons" or "imagine dragons" or artista_3  == "Imagine Dragons"\
         or artista_3  =="imagine dragons":
             play_list.extend(["Believer","Thunder","Natural","Whatever It Takes", "Radioactive"])
@@ -29,13 +35,26 @@ def busca_canciones(artista_1, artista_2, artista_3,play_list):
         else:
             print("Lo siento, no hay datos del artista :(")
 
-def nueva_playlist(nueva_canc):
-    play_list.append(nueva_canc)
-    for v in play_list:
+def nueva_cancion(nueva_canc):
+     """
+    (uso de ciclos for, uso de funciones)
+    recibe: nueva_canc (cadena), nombre de la nueva canción
+    devuelve: la playlist con las canciones previamente añadidas y la nueva canción
+    """
+     play_list.append(nueva_canc)
+     for v in play_list:
             Canciones = v
             print("{:<8}".format(Canciones))
-    add_canc = "%"
+     add_canc = "%"
+    
 def quitar_cancion(no_canc):
+    """
+    (uso de ciclos for,  uso de condicionales, uso de funciones)
+    recibe: no_canc (cadena), nombre de la canción que se quiere eliminar
+    Se busca y borra el nombre de la canción y se quita de la lista play_list.
+    Si no se encuentra se avisa al usuario.
+    devuelve: la playlist con las canciones previamente añadidas, pero sin la canción eliminada
+    """
     if no_canc in play_list:
         play_list.remove(no_canc)
         for v in play_list:
@@ -43,13 +62,21 @@ def quitar_cancion(no_canc):
                 print("{:<8}".format(Canciones))
     else:
         print("La canción no está en esta playlist")
-def obten_lista_de_artista(lista,art_canc):
+
+'''----------------------------VER ARTISTAS----------------------------------'''
+def obten_lista_de_artista(lista_artistas,art_canc):
+    """
+    (uso de ciclos for,  uso de condicionales, uso de funciones)
+    recibe: lista (lista)
+    devuelve: la playlist con las canciones previamente añadidas, pero sin la canción eliminada
+    """
     art_canc = input("Escoge un artista para ver sus canciones:")
     for linea in lista_artistas:
         if art_canc == linea[0]:
             linea.pop(0)
             print(linea)
             
+'''----Listas y variables-----'''
 lista_artistas = [
     ["Imagine Dragons","Believer","Thunder","Natural","Whatever It Takes", "Radioactive"],
     ["Harry Styles", "Adore You","Falling","Lights Up","Sign of Times", "Watermelon Sugar"],
@@ -57,7 +84,13 @@ lista_artistas = [
     ["Kishi Bashi","I am the Anti-Christ to you", "Honeybody","This Must Be The Place","Can't let go Juno","Bright Whites"],
     ["Hans Zimmer", "Cornfield Chase", "Time", "Main Titles: Top Gun Maverick", "Day One", "Darkstar"],
     ["Lorde","Ribs", "Royals", "Team","Liability","Green Light"]]
-print("Hola! Hagamos tu nueva playlist!") 
+play_list = [ ]
+artista_1 = str
+artista_2 = str
+artista_3 = str
+add_canc = str
+''''------------------------------------INICIO DE PROGRAMA--------------------------'''
+print("Hola! Hagamos tu nueva playlist!")
 print("Escoge tres artistas: ")
 print ("{:<8}".format('Artistas'))
 i = 0
@@ -65,17 +98,19 @@ while i < len(lista_artistas):
     Artistas = lista_artistas[i][0]
     print (Artistas)
     i = i + 1
+
+'''---Escoger Artistas---'''
 artista_1 = str(input("Dime tu artista favorito: "))
 artista_2 = str(input("Dime otro artista famoso: "))
 artista_3 = str(input("Dime tu último artista: "))
-play_list = [ ]
 
 
 busca_canciones(artista_1, artista_2, artista_3,play_list)
 for v in play_list:
     Canciones = v
     print ("{:<8}".format(Canciones))
-    
+
+'''----Menú de opciones----'''
 print("¿Añadir otra canción? - A")
 print("¿Quitar una canción?-Q")
 print("Volver a ver los artistas - V")
@@ -83,11 +118,11 @@ print("Si deseas salir: - Exit")
 add_canc = input()
 
 
-while add_canc != "Exit" and add_canc != "exit": #Cambié el while True con break al final por un while que terminara con la palabra Exit. 
+while add_canc != "Exit" and add_canc != "exit": 
     if add_canc == "A" or add_canc == "a":
         nueva_canc = input("Escribe canción: ")
         print("Tu nueva playlist es: ")
-        nueva_playlist(nueva_canc)
+        nueva_cancion(nueva_canc)
         add_canc = "%"
     if add_canc == "Q" or add_canc == "q":
         no_canc = input("¿Cuál canción? ")
@@ -113,4 +148,3 @@ while add_canc != "Exit" and add_canc != "exit": #Cambié el while True con brea
         print("Por favor, ingrese una de las tres opciones")
         add_canc = "%"
         
-
